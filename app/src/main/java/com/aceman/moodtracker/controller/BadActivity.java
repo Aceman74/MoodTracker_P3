cdc2ae9992ed9ba17d96fb7042078870835a4e0e
Aceman74 <thedmx74@hotmail.fr>
2019-01-16 16:05:54 +0100

    Change from fullscreen App to Empty App.

---
+++ app/src/main/java/com/aceman/moodtracker/controller/BadActivity.java
@@ -0,0 +1,15 @@
+package com.aceman.moodtracker.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.aceman.moodtracker.R;
+
+public class BadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bad);
+    }
+}
---
+++ app/src/main/java/com/aceman/moodtracker/controller/VeryHappyActivity.java
@@ -0,0 +1,15 @@
+package com.aceman.moodtracker.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.aceman.moodtracker.R;
+
+public class VeryHappyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_very_happy);
+    }
+}
